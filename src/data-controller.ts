aa72f94888f0f78d4cbee02e7a5e27e7104b16fc
Keagen <keegenjt@gmail.com>
2024-10-13 19:44:16 -0400

    Added functions to html and data-controller

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -1,24 +1,34 @@
 import { Product, products } from "./data";
 
 function generateProductHTML(product: Product): string {
-    return `<div class="store-item">
-              <img src="${product.image}" alt="${product.name}" />
-              <p>${product.name}</p>
-              <p>${product.description}</p>
-              <span>${product.rating}/5</span><span>$${product.price}</span><span>stock ${product.stock}</span>
-            </div>`;
+  return `<div class="store-item">
+            <img src="${product.image}" alt="${product.name}" />
+            <p>${product.name}</p>
+            <p>${product.description}</p>
+            <span>${product.rating}/5</span><span>$${product.price}</span><span>stock ${product.stock}</span>
+          </div>`;
 }
 
 function renderProducts(prods: Product[]): void {
-    // your code
+  const mainContainer = document.getElementById('main-container');
+
+  if (mainContainer) {
+    // Resets html and overwrites
+    mainContainer.innerHTML = "";
+
+    prods.forEach(product => {
+      const productHTML = generateProductHTML(product);
+      mainContainer.innerHTML += productHTML;
+    });
+  }
 }
 
 function getByCategory(category: string): void {
-    // your code
+  return products.filter(product => product.category === category);
 }
 
 function getByRating(minRating: number): void {
-    // your code
+  return products.filter(product => product.rating > minRating);
 }
 
 export { renderProducts, getByCategory, getByRating };
